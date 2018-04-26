3832acf8921fdbc6aaf7c0a0abb82807f40f6b5c
Yan QiDong <yanqd0@outlook.com>
2018-04-26 20:00:21 +0800

    Use setuptools_scm to handle the python package version

--- python/src/leetcode/_meta.py
+++ python/src/leetcode/_meta.py
@@ -3,4 +3,22 @@ __email__ = 'yanqd0@outlook.com'
 __url__ = 'https://github.com/yanqd0/LeetCode'
 __copyright__ = 'Copyright (C) 2018 ' + __author__
 __license__ = 'MIT License'
-__version__ = '0.0.1'
+
+
+def _get_version(default='x.x.x.dev'):
+    try:
+        from pkg_resources import DistributionNotFound, get_distribution
+    except ImportError:
+        return default
+    else:
+        try:
+            return get_distribution(__package__).version
+        except DistributionNotFound:  # Run without install
+            return default
+        except ValueError:  # Python 3 setup
+            return default
+        except TypeError:  # Python 2 setup
+            return default
+
+
+__version__ = _get_version()
