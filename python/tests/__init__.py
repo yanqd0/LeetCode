fd4d3a1ff7a5bccb12f6254edeb52deadbb2d2c7
Yan QiDong <yanqd0@outlook.com>
2018-05-14 21:44:41 +0800

    Share the test cases in Python.

---
+++ python/tests/__init__.py
@@ -0,0 +1,22 @@
+import csv
+import re
+from os.path import abspath, basename, dirname, join
+
+
+def read_csv(path):
+    path = abspath(path)
+    name = basename(path)
+    name = re.sub('^test_', '', name)
+    name = re.sub('py$', 'csv', name)
+    cases = join(dirname(path), 'cases')
+    csv_path = join(cases, name)
+
+    with open(csv_path) as fobj:
+        reader = csv.reader(fobj)
+        result = []
+        for line in reader:
+            if reader.line_num == 1:
+                continue
+            new_line = [eval(item) for item in line]
+            result.append(new_line)
+        return result
