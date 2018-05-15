f5d7e0255e5e2ef001e52be2fe76d01ffca8c1c3
Yan QiDong <yanqd0@outlook.com>
2018-05-15 23:17:27 +0800

    Add csv test for reverse_integer.py

--- python/tests/test_reverse_integer.py
+++ python/tests/test_reverse_integer.py
@@ -1,21 +1,9 @@
 from pytest import mark
 
-from leetcode.reverse_integer import MAX_INT, MIN_INT, Solution
-
-# noinspection SpellCheckingInspection
-PARAMS = [
-    (1, 1),
-    (120, 21),
-    (123, 321),
-    (-123, -321),
-    (1534236469, 0),
-    (9646324351, 0),
-    (MAX_INT, 0),
-    (MIN_INT, 0),
-]
+from leetcode.reverse_integer import Solution
+from . import read_csv
 
 
-@mark.parametrize('x, expect', PARAMS)
+@mark.parametrize('x, expect', read_csv(__file__, parser=int))
 def test_longest_palindrome(x, expect):
-    solution = Solution()
-    assert expect == solution.reverse(x)
+    assert expect == Solution().reverse(x)
---
+++ test/generators/reverse_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+MAX_INT = 2 ** 31 - 1
+MIN_INT = -2 ** 31
+
+fields = ['x', 'expect']
+rows = [
+    (1, 1),
+    (120, 21),
+    (123, 321),
+    (-123, -321),
+    (1534236469, 0),
+    (9646324351, 0),
+    (MAX_INT, 0),
+    (MIN_INT, 0),
+]
+
+if __name__ == '__main__':
+    from __utils import generate_csv
+
+    generate_csv(__file__, fields, rows)
