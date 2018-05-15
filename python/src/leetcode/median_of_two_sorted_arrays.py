9fcb524d8fd4a4bafc106eb02f39e856249a5fbc
Yan QiDong <yanqd0@outlook.com>
2018-05-15 23:14:51 +0800

    Add csv test for median_of_two_sorted_arrays.py

--- python/src/leetcode/median_of_two_sorted_arrays.py
+++ python/src/leetcode/median_of_two_sorted_arrays.py
@@ -17,6 +17,8 @@ nums1 = [1, 2]
 nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
+
+https://leetcode.com/problems/median-of-two-sorted-arrays/
 """
 
 
--- python/tests/test_median_of_two_sorted_arrays.py
+++ python/tests/test_median_of_two_sorted_arrays.py
@@ -1,30 +1,10 @@
 from pytest import mark
 
 from leetcode.median_of_two_sorted_arrays import Solution
+from . import read_csv
 
 
-@mark.parametrize('nums1, nums2, expect', [
-    ([], [1], 1.0),
-    ([2], [], 2.0),
-    ([2], [1, 3, 4], 2.5),
-    ([1, 2, 4], [3], 2.5),
-    ([3], [1, 2], 2.0),
-    ([2], [1, 3, 4, 5], 3.0),
-    ([4], [1, 2, 3, 5], 3.0),
-    ([1, 4], [2, 3, 5, 6, 7], 4.0),
-    ([1, 3], [2], 2.0),
-    ([1, 2], [3, 4], 2.5),
-    ([1, 3], [2, 4], 2.5),
-    ([2, 3], [1, 4], 2.5),
-    ([1], [3], 2.0),
-    ([1], [1, 2, 3, 4], 2.0),
-    ([9], [1, 2, 3, 4], 3.0),
-    ([1, 2, 3, 4, 5, 6], [1, 2], 2.5),
-    ([1, 2, 3, 4, 5, 6], [3, 4], 3.5),
-    ([1, 2, 3, 4, 5, 6], [5, 6], 4.5),
-    ([1, 2, 3, 4, 5, 6], [3, 4, 5, 6, 7, 8, 9], 5.0),
-    ([1, 2, 3, 4, 8, 9], [3, 4, 5, 6, 7, 8, 9], 5.0),
-])
+@mark.parametrize('nums1, nums2, expect', read_csv(__file__))
 def test_find_median_sorted_arrays(nums1, nums2, expect):
     solution = Solution()
     assert expect == solution.findMedianSortedArrays(nums1, nums2)
