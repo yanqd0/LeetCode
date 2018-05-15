1f32f7e003c17dd96870356148208e37818522a8
Yan QiDong <yanqd0@outlook.com>
2018-05-15 18:07:22 +0800

    Add csv test for AddTwoNumbers.java

--- java/src/test/java/name/qidong/leetcode/test/AddTwoNumbersTest.java
+++ java/src/test/java/name/qidong/leetcode/test/AddTwoNumbersTest.java
@@ -2,7 +2,12 @@ package name.qidong.leetcode.test;
 
 import name.qidong.leetcode.AddTwoNumbers;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ArgumentConverter;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static name.qidong.leetcode.AddTwoNumbers.ListNode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,47 +20,48 @@ class AddTwoNumbersTest {
         solution = new AddTwoNumbers();
     }
 
-    @Test
-    void addTwoNumbers0() {
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-
+    @ParameterizedTest
+    @CsvFileSource(resources = "/add_two_numbers.csv", numLinesToSkip = 1)
+    void addTwoNumbers(
+            @ConvertWith(String2ListNode.class) ListNode l1,
+            @ConvertWith(String2ListNode.class) ListNode l2,
+            @ConvertWith(String2ListNode.class) ListNode expect
+    ) {
+        solution = new AddTwoNumbers();
         ListNode result = solution.addTwoNumbers(l1, l2);
-        assertEquals(7, result.val);
-        assertEquals(0, result.next.val);
-        assertEquals(8, result.next.next.val);
-    }
-
-    @Test
-    void addTwoNumbers1() {
-        ListNode l1 = new ListNode(5);
-        ListNode l2 = new ListNode(5);
-        ListNode result = solution.addTwoNumbers(l1, l2);
-        assertEquals(0, result.val);
-        assertEquals(1, result.next.val);
-    }
-
-    @Test
-    void addTwoNumbers2() {
-        ListNode l1 = new ListNode(9);
-        ListNode cursor = l1;
-        for (int i = 0; i < 9; ++i) {
-            cursor.next = new ListNode(9);
-            cursor = cursor.next;
+        while (expect != null || result != null) {
+            assertEquals(expect.val, result.val);
+            expect = expect.next;
+            result = result.next;
         }
-        ListNode l2 = new ListNode(1);
+    }
+}
 
-        cursor = solution.addTwoNumbers(l1, l2);
-
-        for (int i = 0; i < 10; ++i) {
-            assertEquals(0, cursor.val);
-            cursor = cursor.next;
+class String2ListNode implements ArgumentConverter {
+    @Override
+    public Object convert(Object source, ParameterContext context)
+            throws ArgumentConversionException {
+        try {
+            String str = (String) source;
+            final String sep = "->";
+            if (str.contains(sep)) {
+                ListNode root = null, last = null;
+                for (String value : str.split(sep)) {
+                    ListNode node = new ListNode(Integer.parseInt(value.trim()));
+                    if (last == null) {
+                        root = last = node;
+                    } else {
+                        last = last.next = node;
+                    }
+                }
+                return root;
+            } else {
+                return new ListNode(Integer.parseInt(str.trim()));
+            }
+        } catch (ClassCastException e) {
+            throw new ArgumentConversionException("The source is not a String", e);
+        } catch (NumberFormatException e) {
+            throw new ArgumentConversionException("Some content in source is not int", e);
         }
-        assertEquals(1, cursor.val);
     }
 }
