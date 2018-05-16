739168a91a01505d6d95c4d5b0364e14c3e231a9
Yan QiDong <yanqd0@outlook.com>
2018-05-16 16:07:24 +0800

    Use csv test for ZigZagConversion.java

--- java/src/test/java/name/qidong/leetcode/test/ZigZagConversionTest.java
+++ java/src/test/java/name/qidong/leetcode/test/ZigZagConversionTest.java
@@ -1,38 +1,19 @@
 package name.qidong.leetcode.test;
 
 import name.qidong.leetcode.ZigZagConversion;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ZigZagConversionTest {
-    private ZigZagConversion solution;
-
-    @BeforeEach
-    void setUp() {
-        solution = new ZigZagConversion();
-    }
-
-    @SuppressWarnings("SpellCheckingInspection")
-    @ParameterizedTest(name = "{index} => s={0}, expect={1}")
-    @CsvSource({
-            "'', 0, ''",
-            "'', 1, ''",
-            "'ABC', 0, 'ABC'",
-            "'ABC', 1, 'ABC'",
-            "'PAYPALISHIRING', 0, 'PAYPALISHIRING'",
-            "'PAYPALISHIRING', 1, 'PAYPALISHIRING'",
-            "'PAYPALISHIRING', 2, 'PYAIHRNAPLSIIG'",
-            "'PAYPALISHIRING', 3, 'PAHNAPLSIIGYIR'",
-            "'abcdefghijklmn', 4, 'agmbfhlnceikdj'",
-            "'abcdefghijklmn', 5, 'aibhjcgkdflnem'",
-            "'abcdefghijklmno', 5, 'aibhjcgkodflnem'",
-            "'abcdefghijklmnop', 5, 'aibhjpcgkodflnem'",
-            "'abcde', 5, 'abcde'",
-    })
+    @ParameterizedTest
+    @CsvFileSource(resources = "/zigzag_conversion.csv", numLinesToSkip = 1)
     void convert(String s, int numRows, String expect) {
+        ZigZagConversion solution = new ZigZagConversion();
+        if (s == null) s = "";
+        if (expect == null) expect = "";
+
         assertEquals(expect, solution.convert(s, numRows));
     }
 }
