653a1f027211f62da33397edb72d20949690873b
Yan QiDong <yanqd0@outlook.com>
2018-05-16 15:56:29 +0800

    Use csv for ReverseInteger.java

--- java/src/test/java/name/qidong/leetcode/test/ReverseIntegerTest.java
+++ java/src/test/java/name/qidong/leetcode/test/ReverseIntegerTest.java
@@ -1,31 +1,16 @@
 package name.qidong.leetcode.test;
 
 import name.qidong.leetcode.ReverseInteger;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReverseIntegerTest {
-    private ReverseInteger solution;
-
-    @BeforeEach
-    void setUp() {
-        solution = new ReverseInteger();
-    }
-
-    @ParameterizedTest(name = "{index} => s={0}, expect={1}")
-    @CsvSource({
-            "1, 1",
-            "120, 21",
-            "123, 321",
-            "-123, -321",
-            "1534236469, 0",
-            "2147483642, 0",
-            "-2147483648, 0",
-    })
+    @ParameterizedTest
+    @CsvFileSource(resources = "/reverse_integer.csv", numLinesToSkip = 1)
     void reverse(int x, int expect) {
+        ReverseInteger solution = new ReverseInteger();
         assertEquals(expect, solution.reverse(x));
     }
 }
