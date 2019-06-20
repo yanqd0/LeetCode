d01ed89a9ce1e08003f3d2405e0d3da4bb6a3f8e
Yan QiDong <yanqd0@outlook.com>
2019-06-20 22:53:28 +0800

    Rename twoSum.spec.js

--- javascript/tests/unit/twoSum.spec.js
+++ javascript/tests/unit/twoSum.spec.js
@@ -3,12 +3,12 @@ import twoSum from '../../src/leetcode/twoSum';
 
 const CASES = readCases('two_sum.csv');
 
-describe('TwoSum.js', () => {
+describe('twoSum.js', () => {
   test.each(CASES)('nums=%s, target=%s, expect=%s', (nums, target, expected) => {
     expect(expected).toEqual(twoSum(nums, target));
   });
 
-  test('Invalid input', () => {
+  test('twoSum.js invalid input', () => {
     expect(() => twoSum([1, 2], 4)).toThrow(Error);
   });
 });
