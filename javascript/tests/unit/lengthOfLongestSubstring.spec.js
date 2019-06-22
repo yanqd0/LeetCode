35c709fba4301e2580dab0a3db1573114e021292
Yan QiDong <yanqd0@outlook.com>
2019-06-22 22:36:28 +0800

    Fix comments for p3 js test

--- javascript/tests/unit/lengthOfLongestSubstring.spec.js
+++ javascript/tests/unit/lengthOfLongestSubstring.spec.js
@@ -1,10 +1,9 @@
 import { readCases } from './utils';
 import lengthOfLongestSubstring from '../../src/leetcode/lengthOfLongestSubstring';
 
-// eslint-disable-next-line no-eval
 const CASES = readCases('longest_substring_without_repeating_characters.csv', [String, Number]);
 
-describe('addTwoNumbers.js', () => {
+describe('lengthOfLongestSubstring.js', () => {
   test.each(CASES)('s=%s, expect=%s', (s, expected) => {
     expect(expected).toEqual(lengthOfLongestSubstring(s));
   });
